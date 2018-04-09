fe9ed6a146c6568f629af0b9d204476a6c740fa5
Rob Morgan <rmorgan323@gmail.com>
2018-04-08 21:18:37 -0600

    Add routing and about page

---
+++ src/components/About.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="about">
+    <h1>Hello, {{ user_name }}, this is an about page.</h1>
+    <p>Some stuff that's great and belongs on an about page</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Skills',
+  data() {
+    return {
+      user_name: this.$route.params.name
+    }
+  }
+}
+</script>
--- src/components/Skills.vue
+++ src/components/Skills.vue
@@ -1,8 +1,22 @@
 <template>
   <div class="hello">
     <div class="holder">
+
+      <form @submit.prevent="addSkill">
+        <input type="text" placeholder="Enter a skill you have..." v-model="skill" v-validate="'min:5'" name="skill">
+
+        <transition name="alert-in" enter-active-class="animated flipInX" leave-active-class="animated flipOutX">
+          <p class="alert" v-if="errors.has('skill')">{{ errors.first('skill') }}</p>
+        </transition>
+      </form>
+
       <ul>
-        <li v-for="(data, index) in skills" :key='index'>{{ data.skill }}</li>
+        <transition-group name="list" enter-active-class="animated bounceInUp" leave-active-class="animated bounceOutDown">
+          <li v-for="(data, index) in skills" :key='index'>
+            {{ data.skill }}
+            <i class="fa fa-minus-circle" v-on:click="deleteSkill(index)"></i>
+          </li>
+        </transition-group>
       </ul>
 
       <p>These are the skills that you possess.</p>
@@ -15,10 +29,26 @@ export default {
   name: 'Skills',
   data() {
     return {
+      skill: '',
       skills: [
         { skill: "Vue.js" },
         { skill: "Front End Developer" }
       ]
+    }
+  },
+  methods: {
+    addSkill() {
+      this.$validator.validateAll().then((result) => {
+        if (result) {
+          this.skills.push({ skill: this.skill });
+          this.skill = '';
+        } else {
+          console.log('Not Valid')
+        }
+      })
+    },
+    deleteSkill(index) {
+      this.skills.splice(index, 1);
     }
   }
 }
@@ -26,6 +56,8 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+  @import "https://cdn.jsdelivr.net/npm/animate.css@3.5.1";
+  @import "https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css";
 
   .holder {
     background: #fff;
@@ -54,6 +86,49 @@ export default {
 
   .container {
     box-shadow: 0px 0px 40px lightgrey;
+  }
+
+  input {
+    width: calc(100% - 40px);
+    border: 0;
+    padding: 20px;
+    font-size: 1.3em;
+    background-color: #323333;
+    color: #687f7f;
+    outline: none;
+  }
+
+  .alert {
+    background: #fdf2ce;
+    font-weight: bold;
+    display: inline-block;
+    padding: 5px;
+    margin-top: -20px;
+  }
+
+  .alert-in-enter-active {
+    animation: bounce-in .5s;
+  }
+
+  .alert-in-leave-active {
+    animation: bounce-in .5s reverse;
+  }
+  
+  @keyframes bounce-in {
+    0% {
+      transform: scale(0);
+    }
+    50% {
+      transform: scale(1.5);
+    }
+    100% {
+      transform: scale(1);
+    }
+  }
+
+  i {
+    float: right;
+    cursor: pointer;
   }
 
 </style>
